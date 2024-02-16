f57f95d179dc940aca2f4ee924c90229af95748e
Wander Alves <wsilva_94@outlook.com>
2024-02-16 14:01:40 -0300

    fix: add env region

--- src/libs/SESClient/config/ses.ts
+++ src/libs/SESClient/config/ses.ts
@@ -1,7 +1,7 @@
 import IConfig from '../interfaces/IConfig';
 
 const config = {
-    region: 'us-east-1',
+    region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
